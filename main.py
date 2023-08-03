134fee69fc7776d5961212a5623fb9d692134e42
Francis Isip <francis_bawa@dlsu.edu.ph>
2023-08-03 16:15:03 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import tkinter
+
+
+# variable initialization
+nStates = 0
+setOfStates = ["q0"]
+inputAlphabet = []
+stackAlphabet = []
+transitions = []
+# leftEndmarker = "¢" // not sure if needed
+# rightEndMarker = "$" // not sure if needed
+initialState = ""
+finalStates = []
+rejectedStates = []
